414659d6b2bf8b43dd6728b18fab1a34835d6820
H2ofiremaster <75271196+H2ofiremaster@users.noreply.github.com>
2026-01-04 14:14:38 -0800

    Added image randomization logic

---
+++ src/puppyrng.js
@@ -0,0 +1,28 @@
+const PUPPIES = [
+    "https://imgur.com/two-puppies-wearing-top-hats-40nPh",
+    "https://images.puppies.com/processed/fba5b9b3-2513-44a0-a61c-09e4aa67de80/listing/a0ad617a-1e67-4c9f-8e71-7d89f87db8fe/76d8573b-4a3a-4fde-9ad6-99fbf791d352-opt.webp",
+    "https://preview.redd.it/adopted-this-little-dude-today-needs-a-name-12-weeks-old-v0-vyl964z4xabg1.jpg?width=1080&crop=smart&auto=webp&s=f242397a4182485ce4a76ac13b1a590d707a6e4b",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/250px-Golde33443.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Cara_de_quem_caiu_do_caminh%C3%A3o..._%28cropped%29.jpg/250px-Cara_de_quem_caiu_do_caminh%C3%A3o..._%28cropped%29.jpg",
+    "https://eskipaper.com/images/adorable-puppies-1.jpg",
+    "https://i.ytimg.com/vi/PqweYCDi3uc/maxresdefault.jpg",
+    "http://www.iheartteacups.com/wp-content/uploads/2017/11/IMG_6958.jpg",
+    "http://www.iheartteacups.com/wp-content/uploads/2016/09/IMG_7801.jpg",
+    "https://miniteacuppups.com/wp-content/uploads/2020/11/Soonyi-Female-Teacup-Morkie-1-1024x1024.jpg",
+    "https://wallpapers.com/images/hd/cute-puppies-pictures-iq9esw6nily31v5g.jpg",
+    "https://i.pinimg.com/originals/87/07/cf/8707cff9dfdaf60143317263a20d6909.jpg",
+    "https://i.pinimg.com/originals/a8/20/f6/a820f6e2b86f678febc855972223bc6a.jpg"
+];
+
+const PUPPY_IMAGE = document.getElementById("puppy-image");
+const BARK = new Audio("../assets/dog-bark.mp3");
+PUPPY_IMAGE.addEventListener("click", () => {
+    let current_image = PUPPY_IMAGE.getAttribute("src");
+    var new_image = PUPPIES[Math.floor((Math.random() * PUPPIES.length))];
+    while (new_image == current_image) {
+        PUPPIES.random()
+        new_image = PUPPIES[Math.floor((Math.random() * PUPPIES.length))];
+    }
+    PUPPY_IMAGE.setAttribute("src", new_image);
+    BARK.play();
+})
